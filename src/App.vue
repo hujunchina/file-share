11da5c08abc5bbb04931ea7381667a451e26e9b4
hujunchina <hujunchina@outlook.com>
2020-08-24 15:50:56 +0800

    feat: add upload file element

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <FileShare/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import FileShare from './components/FileShare.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    FileShare,
   }
 }
 </script>
---
+++ src/components/FileShare.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-upload
+    class="upload-demo"
+    drag
+    action="http://www.so-paper.com:8081/posts/"
+    multiple>
+    <i class="el-icon-upload"></i>
+    <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+    <div class="el-upload__tip" slot="tip">上传文件会生成二维码或易记的URL</div>
+  </el-upload>
+</template>
+
+<script>
+export default {
+  name: 'FileShare',
+  props: {
+    msg: String
+  }
+}
+</script>
